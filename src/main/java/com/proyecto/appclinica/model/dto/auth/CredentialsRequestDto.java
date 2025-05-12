c04901300bee8856b19049aeb1293a1b89c66c5b
Jancarlos042003 <U21302940@utp.edu.pe>
2025-05-12 18:10:08 -0500

    Add JsonProperty annotation to confirmPassword in CredentialsRequestDto

--- src/main/java/com/proyecto/appclinica/model/dto/auth/CredentialsRequestDto.java
+++ src/main/java/com/proyecto/appclinica/model/dto/auth/CredentialsRequestDto.java
@@ -1,5 +1,6 @@
 package com.proyecto.appclinica.model.dto.auth;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,5 +17,6 @@ public class CredentialsRequestDto {
     private String password;
 
     @NotBlank(message = "La confirmación de la contraseña es obligatoria")
+    @JsonProperty("confirm_password")
     private String confirmPassword;
 }
