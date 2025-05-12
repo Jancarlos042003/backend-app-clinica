7237466c8fbf45466b65cfac4549e993261553cc
Jancarlos042003 <U21302940@utp.edu.pe>
2025-05-12 18:06:08 -0500

    Refactor AuthController to update user existence check response and add login endpoint

--- src/main/java/com/proyecto/appclinica/controller/AuthController.java
+++ src/main/java/com/proyecto/appclinica/controller/AuthController.java
@@ -1,12 +1,10 @@
 package com.proyecto.appclinica.controller;
 
 import com.proyecto.appclinica.model.dto.IdentifierRequest;
-import com.proyecto.appclinica.model.dto.auth.CodeSubmissionResponseDto;
-import com.proyecto.appclinica.model.dto.auth.ResponseUserExistsDto;
-import com.proyecto.appclinica.model.dto.auth.VerifyCodeRequest;
-import com.proyecto.appclinica.model.dto.auth.VerifyCodeResponse;
+import com.proyecto.appclinica.model.dto.auth.*;
 import com.proyecto.appclinica.service.impl.AuthServiceImpl;
 import com.proyecto.appclinica.service.CodeVerificationService;
+import com.proyecto.appclinica.service.impl.LoginServiceImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,20 +15,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthServiceImpl authService;
+    private final LoginServiceImpl loginService;
     private final CodeVerificationService codeService;
 
     @GetMapping("/check-user")
-    public ResponseEntity<ResponseUserExistsDto> checkUserExists(@Valid @RequestBody IdentifierRequest request) {
+    public ResponseEntity<CodeSubmissionResponseDto> checkUserExists(@Valid @RequestBody IdentifierRequest request) {
         return ResponseEntity.ok(authService.checkUserExists(request.getIdentifier()));
-    }
-
-    @PostMapping("/send-code")
-    public ResponseEntity<CodeSubmissionResponseDto> sendVerificationCode(@Valid @RequestBody IdentifierRequest request) {
-        return ResponseEntity.ok(codeService.generateAndSendCode(request.getIdentifier()));
     }
 
     @PostMapping("/verify-code")
     public ResponseEntity<VerifyCodeResponse> verifyCode(@Valid @RequestBody VerifyCodeRequest request) {
         return ResponseEntity.ok(authService.verifyCode(request.getIdentifier(), request.getCode()));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDto> login(@Valid @RequestBody LoginRequestDto request) {
+        return ResponseEntity.ok(loginService.login(request));
     }
 }
