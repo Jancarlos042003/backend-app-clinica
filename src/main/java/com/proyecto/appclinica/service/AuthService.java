bdc4ba4ab9b27f3fb451813c4830a3e046605a4a
Jancarlos042003 <U21302940@utp.edu.pe>
2025-05-12 18:07:08 -0500

    Refactor AuthService to update user existence check response type

--- src/main/java/com/proyecto/appclinica/service/AuthService.java
+++ src/main/java/com/proyecto/appclinica/service/AuthService.java
@@ -1,9 +1,9 @@
 package com.proyecto.appclinica.service;
 
-import com.proyecto.appclinica.model.dto.auth.ResponseUserExistsDto;
+import com.proyecto.appclinica.model.dto.auth.CodeSubmissionResponseDto;
 import com.proyecto.appclinica.model.dto.auth.VerifyCodeResponse;
 
 public interface AuthService {
-    ResponseUserExistsDto checkUserExists(String identifier);
+    CodeSubmissionResponseDto checkUserExists(String identifier);
     VerifyCodeResponse verifyCode(String identifier, String code);
 }
