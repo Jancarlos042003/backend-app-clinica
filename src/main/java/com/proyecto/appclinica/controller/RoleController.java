1fb1e213fbc12cc2521cc7ea0815ea42ffe9d7b0
Jancarlos042003 <U21302940@utp.edu.pe>
2025-05-12 18:07:46 -0500

    Add role-based access control

--- src/main/java/com/proyecto/appclinica/controller/RoleController.java
+++ src/main/java/com/proyecto/appclinica/controller/RoleController.java
@@ -6,6 +6,7 @@ import com.proyecto.appclinica.service.impl.RoleServiceImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
@@ -16,29 +17,34 @@ import java.util.Set;
 public class RoleController {
     private final RoleServiceImpl roleService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<String> createRole(@Valid @RequestBody RoleRequestDto requestDto) {
         roleService.createRole(requestDto);
         return ResponseEntity.ok("Rol creado correctamente");
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/update/{id}")
     public ResponseEntity<String> updateRole(@Valid @RequestBody RoleRequestDto requestDto, @PathVariable Long id) {
         roleService.update(requestDto, id);
         return ResponseEntity.ok("Rol actualizado correctamente");
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteRole(@PathVariable Long id) {
         roleService.delete(id);
         return ResponseEntity.ok("Rol eliminado correctamente");
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public ResponseEntity<Set<RoleEntity>> getAllRoles() {
         return ResponseEntity.ok(roleService.getAllRoles());
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<RoleEntity> getRoleById(@PathVariable Long id) {
         return ResponseEntity.ok(roleService.findById(id));
