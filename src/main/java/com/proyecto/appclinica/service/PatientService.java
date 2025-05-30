f652e0aa83e5d5f8c68cd8b76cd521e8b35959c9
Jancarlos042003 <U21302940@utp.edu.pe>
2025-05-30 01:36:26 -0500

    Rename UserService to PatientService

--- src/main/java/com/proyecto/appclinica/service/PatientService.java
+++ src/main/java/com/proyecto/appclinica/service/PatientService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class PatientService {
     private final FhirPatientRepository fhirPatientRepository;
 
     public UserProfileResponse getUserProfile(String identifier) {
