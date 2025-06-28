575062dbe99b177e033797425a2846c0358d4b39
Jancarlos042003 <U21302940@utp.edu.pe>
2025-06-28 09:17:23 -0500

    Add medication check scheduler for periodic medication verification

--- src/main/java/com/proyecto/appclinica/AppClinicaApplication.java
+++ src/main/java/com/proyecto/appclinica/AppClinicaApplication.java
@@ -2,8 +2,10 @@ package com.proyecto.appclinica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // Habilita la programación de tareas
 public class AppClinicaApplication {
 
     public static void main(String[] args) {
