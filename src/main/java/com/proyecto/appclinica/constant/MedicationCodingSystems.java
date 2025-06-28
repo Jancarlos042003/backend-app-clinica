276db7378b1e90ac73cd840cb0e75a5bad9a3544
Jancarlos042003 <U21302940@utp.edu.pe>
2025-06-28 09:48:59 -0500

    Remove unused medication coding system constants from MedicationCodingSystems

--- src/main/java/com/proyecto/appclinica/constant/MedicationCodingSystems.java
+++ src/main/java/com/proyecto/appclinica/constant/MedicationCodingSystems.java
@@ -8,15 +8,6 @@ public class MedicationCodingSystems {
     /** Sistema RxNorm para la codificación de medicamentos */
     public static final String RXNORM = "http://www.nlm.nih.gov/research/umls/rxnorm";
 
-    /** Sistema SNOMED CT para la codificación de medicamentos */
-    public static final String SNOMED_CT = "http://snomed.info/sct";
-
-    /** Sistema ATC (Anatomical Therapeutic Chemical) para la codificación de medicamentos */
-    public static final String ATC = "http://www.whocc.no/atc";
-
-    /** Sistema NDC (National Drug Code) para la codificación de medicamentos */
-    public static final String NDC = "http://hl7.org/fhir/sid/ndc";
-
     private MedicationCodingSystems() {
         // Constructor privado para evitar la instanciación
     }
